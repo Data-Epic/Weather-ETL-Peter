c13ead30e31e4e0197921c6ad5ecd0d02c9caea2
unknown <peteradekolu@gmail.com>
2024-09-26 13:12:58 +0100

    adding src config file

--- dags/config.py
+++ dags/config.py
@@ -1,10 +1,13 @@
+import logging
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import logging
-from dags.logger_config import log_file_path, error_log_file_path
-from typing import List
+
 from airflow.models import Variable
+
+from dags.logger_config import error_log_file_path, log_file_path
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 # set up logging
 logging.basicConfig(
@@ -28,28 +31,36 @@ FIELDS = os.getenv("FIELDS")
 WEATHER_FIELDS_EXCLUDE = os.getenv("WEATHER_FIELDS_EXCLUDE")
 DATABASE_URL = os.getenv("DATABASE_URL")
 COUNTRY_NAMES = os.getenv("COUNTRY_NAMES")
+START_DATE_YEAR = os.getenv("START_DATE_YEAR")
+END_DATE_YEAR = os.getenv("END_DATE_YEAR")
 
 AIRFLOW_API_KEY = Variable.get("API_KEY", default_var=API_KEY)
 AIRFLOW_CITY_NAMES = Variable.get("CITY_NAMES", default_var=CITY_NAMES)
 AIRFLOW_COUNTRY_NAMES = Variable.get("COUNTRY_NAMES", default_var=COUNTRY_NAMES)
 AIRFLOW_FIELDS = Variable.get("FIELDS", default_var=FIELDS)
-AIRFLOW_WEATHER_FIELDS_EXCLUDE = Variable.get("WEATHER_FIELDS_EXCLUDE", default_var=WEATHER_FIELDS_EXCLUDE)
+AIRFLOW_WEATHER_FIELDS_EXCLUDE = Variable.get(
+    "WEATHER_FIELDS_EXCLUDE", default_var=WEATHER_FIELDS_EXCLUDE
+)
 AIRFLOW_DATABASE_URL = Variable.get("DATABASE_URL", default_var=DATABASE_URL)
+AIRFLOW_START_DATE_YEAR = Variable.get("START_DATE_YEAR", default_var=START_DATE_YEAR)
+AIRFLOW_END_DATE_YEAR = Variable.get("END_DATE_YEAR", default_var=END_DATE_YEAR)
 
-def process_var(var:list) -> list:
+
+def process_var(var: str) -> list:
     """
-    Process the environmental variable from a string to a list of strings if 
-    there is a comma in the string
-    
-    Args: var(list) 
-    
-    returns: list (A list of environmental variables)
+    Process the environmental variable from a string to a list of strings if
+    there is a comma in the varable.
+
+    Args: var(str) An environmental variable.
+
+    returns: var(list) (A list)
     """
     if "," in var:
         return var.split(",")
-    
+
     else:
         return var
+
 
 CITY_NAMES = process_var(CITY_NAMES)
 FIELDS = process_var(FIELDS)
